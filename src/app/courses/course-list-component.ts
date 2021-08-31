83d8c4840de3f4f6e4f198dcbcc632fdad983b5b
rafaelmocorrea <rafaelmocorrea@gmail.com>
2021-08-31 19:00:01 -0300

    star-selector

--- src/app/courses/course-list-component.ts
+++ src/app/courses/course-list-component.ts
@@ -14,21 +14,21 @@ export class CourseListComponent implements OnInit{
             {
                 id: 1,
                 name: 'Curso de Angular: Forms',
-                imgUrl: '',
+                imgUrl: '/assets/images/forms.png',
                 price: 100,
                 code: 'CRS-0001',
                 duration: 120,
-                rating: 5.5,
+                rating: 4.5,
                 releaseDate: 'August, 31, 2021'
             },
             {
                 id: 2,
-                name: 'Curso de Angular: HTML',
-                imgUrl: '',
+                name: 'Curso de Angular: HTTP',
+                imgUrl: '/assets/images/http.png',
                 price: 50,
                 code: 'CRS-0002',
                 duration: 150,
-                rating: 5.8,
+                rating: 3.8,
                 releaseDate: 'September, 1, 2021'
             }
         ]
